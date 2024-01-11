08c1e4c6c4eb9b55ddf39afe48904b3eb2d70041
geo <geoc2022@gmail.com>
2024-01-11 00:57:49 -0500

    Fixed Mistake with Field Def

--- Game/MyAlgebra/Ring_Def.lean
+++ Game/MyAlgebra/Ring_Def.lean
@@ -6,4 +6,7 @@ class Ring (R : Type) extends AddCommGroup R, Monoid R :=
   distrib_left : ∀ a b c : R, a * (b + c) = a * b + a * c
   distrib_right : ∀ a b c : R, (a + b) * c = a * c + b * c
 
+class CommRing (R : Type) extends Ring R :=
+  mul_comm : ∀ a b : R, a * b = b * a
+
 export Ring (distrib_left distrib_right)
