0cc74182e902df274623637e924acc02e6a6dd0f
geo <geoc2022@gmail.com>
2024-01-16 17:20:50 -0500

    Clean up proofs

--- Game/Levels/GroupBasics/L09_InvInv.lean
+++ Game/Levels/GroupBasics/L09_InvInv.lean
@@ -17,16 +17,16 @@ Introduction "
 TheoremDoc MyAlgebra.inv_inv as "inv_inv" in "Group"
 @[to_additive]
 Statement inv_inv (a : G) [Group G] : a⁻¹⁻¹ = a := by
-  have h : a⁻¹⁻¹ * a⁻¹ = 1 := by
-    rw [inv_mul]
-  have h2 : (a⁻¹⁻¹ * a⁻¹) * a = 1 * a := by
-    rw [h]
-  have h3 : a⁻¹⁻¹ * (a⁻¹ * a) = a := by
-    rw [one_mul] at h2
-    rw [mul_assoc] at h2
-    exact h2
-  rw [inv_mul] at h3
-  rw [mul_one] at h3
-  exact h3
+  apply Eq.symm
+  calc a = a := by rfl
+    _ = a⁻¹⁻¹ * a⁻¹ * a := by
+      rw [inv_mul]
+      rw [one_mul]
+    _ = a⁻¹⁻¹ * (a⁻¹ * a) := by
+      rw [mul_assoc]
+    _ = a⁻¹⁻¹ * 1 := by
+      rw [inv_mul]
+    _ = a⁻¹⁻¹ := by
+      rw [mul_one]
 
 Conclusion "Congrats!"
