55585b3753f0dc8f7c2270e17059d347adf3e5a2
geo <geoc2022@gmail.com>
2024-01-27 15:07:31 -0500

    Add hints to GroupHom

--- Game/Levels/GroupHom/L01_HomPreservesOne.lean
+++ Game/Levels/GroupHom/L01_HomPreservesOne.lean
@@ -25,6 +25,8 @@ Introduction "
 TheoremDoc MyAlgebra.hom_preserves_one as "hom_preserves_one" in "Group"
 -- @[to_additive]
 Statement hom_preserves_one [Group G] [Group H] (f : GroupHom G H) : f 1 = 1 := by
+  Hint "We probably want to use f.hom with terms that have f 1 to match with the goal"
+  Hint "Remmeber that f.hom is a function that takes two arguments"
   have q := f.hom 1 1
   rw [one_mul] at q
   apply mul_right_cancel (f 1)
