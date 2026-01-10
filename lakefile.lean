9ffe2fb3df66e719b506cc08a5e74ed7a2d041b7
Geoc2022 <geoc2022@gmail.com>
2026-01-10 11:25:06 -0500

    Add mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -51,7 +51,7 @@ require "leanprover-community" / mathlib @ git leanVersion
 ```
  -/
 
--- require "leanprover-community" / mathlib @ git leanVersion
+require "leanprover-community" / mathlib @ git leanVersion
 
 /-!
 # PACKAGE CONFIGURATION
